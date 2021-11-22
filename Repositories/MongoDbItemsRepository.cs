2d02a13067b1963988083cae1580d6e32f7983ae
Wuerike <wuerike.henri@gmail.com>
2021-11-22 11:49:42 -0300

    Add mongodb repository

---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,51 @@
+using Catalog_api.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Catalog_api.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+
+        private const string databaseName = "catalog-api";
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            itemsCollection = database.GetCollection<Item>(collectionName);
+
+        }
+
+        public async Task CreateItemAsync(Item item)
+        {
+            await itemsCollection.InsertOneAsync(item);
+        }
+
+        public async Task DeleteItemAsync(Guid id)
+        {
+            var filter = filterBuilder.Eq(actualItem => actualItem.Id, id);
+            await itemsCollection.DeleteOneAsync(filter);
+        }
+
+        public async Task<Item?> GetItemAsync(Guid id)
+        {
+            var filter = filterBuilder.Eq(actualItem => actualItem.Id, id);
+            return await itemsCollection.Find(filter).SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync()
+        {
+            return await itemsCollection.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public async Task UpdateItemAsync(Item item)
+        {
+            var filter = filterBuilder.Eq(actualItem => actualItem.Id, item.Id);
+            await itemsCollection.ReplaceOneAsync(filter, item);
+        }
+    }
+
+}
