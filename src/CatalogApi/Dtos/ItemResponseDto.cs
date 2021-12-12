3391070835db57269a12c258dc34316dc13a6671
Wuerike <wuerike.henri@gmail.com>
2021-12-12 17:39:02 -0300

    Fix ItemResponseDto CreatedAt type

--- src/CatalogApi/Dtos/ItemResponseDto.cs
+++ src/CatalogApi/Dtos/ItemResponseDto.cs
@@ -5,6 +5,6 @@ namespace CatalogApi.Dtos
         public Guid Id {get; init;}
         public string? Name { get; init; }
         public decimal Price { get; init; }
-        public DateTimeOffset CreatedAt { get; init; }
+        public DateTime CreatedAt { get; init; }
     }
 }
